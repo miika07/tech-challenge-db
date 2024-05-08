8259c20a31699bb7dd310ea5b9ebeb5e0988ce80
Camila Couto <camilagcouto@hotmail.com>
2024-05-08 00:06:11 -0300

    build test

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "database" {
   engine               = "mysql"
   engine_version       = "5.7"
   storage_type         = "gp2"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t3.micro"
   username             = "fiap"
   password             = "password"
   parameter_group_name = "default.mysql5.7"
