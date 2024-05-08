335e540f4b5d92c14daf899157f314b55da9bfab
Camila Couto <camilagcouto@hotmail.com>
2024-05-07 23:55:32 -0300

    primeiro commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+#---------------------------------------------------------------------
+# Infraestrutura como código para o database do projeto Tech Challenge
+#---------------------------------------------------------------------
+resource "aws_db_instance" "database" {
+  allocated_storage    = 10
+  db_name              = "tech-challenge-fiap-terraform"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  storage_type         = "gp2"
+  instance_class       = "db.t2.micro"
+  username             = "fiap"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+}
