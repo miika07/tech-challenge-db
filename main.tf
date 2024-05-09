4e264b28e54d9239ddfe6ba0cc3034860cc1568e
Camila Couto <camilagcouto@hotmail.com>
2024-05-08 22:19:28 -0300

    fiz secrets

--- main.tf
+++ main.tf
@@ -2,14 +2,18 @@
 # Infraestrutura como código para o database do projeto Tech Challenge
 #---------------------------------------------------------------------
 resource "aws_db_instance" "database" {
-  identifier           = "tech-challenge-fiap"
-  allocated_storage    = 10
-  db_name              = "techchallenge"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  storage_type         = "gp2"
-  instance_class       = "db.t3.micro"
-  username             = "fiap"
-  password             = "password"
-  parameter_group_name = "default.mysql5.7"
+  identifier            = "tech-challenge-fiap"
+  allocated_storage     = 20
+  max_allocated_storage = 20
+  db_name               = "techchallenge"
+  engine                = "mysql"
+  engine_version        = "8.0"
+  storage_type          = "gp3"
+  instance_class        = "db.t3.micro"
+  username              = var.dbUsername
+  password              = var.dbPassword
+  parameter_group_name  = "default.mysql8.0"
+  skip_final_snapshot   = true
+  publicly_accessible   = true
+  multi_az              = false
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "dbUsername" {
+  type        = string
+  description = "Username do banco de dados"
+}
+
+variable "dbPassword" {
+  type        = string
+  description = "Senha do banco de dados"
+}
