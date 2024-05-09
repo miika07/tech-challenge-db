03468b6cecf4af499c556eeeaab35cd838952d50
Camila Couto <camilagcouto@hotmail.com>
2024-05-08 22:50:52 -0300

    gogo

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "dbUsername" {
   type        = string
-  description = "Username do banco de dados"
+  description = "Username do banco de dados."
 }
 
 variable "dbPassword" {
   type        = string
-  description = "Senha do banco de dados"
+  description = "Senha do banco de dados."
 }
